b2862ea16160fc6597961a1ef1dbcd1b69724239
QuanLi <quanlidavid@gmail.com>
2022-02-02 14:52:45 +0800

    use a search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,6 @@
 console.log('Client side javascript file is loaded!');
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+/* fetch('http://puzzle.mead.io/puzzle').then((response) => {
 	response.json().then((data) => {
 		console.log(data);
 	});
@@ -19,5 +19,29 @@ fetch('http://localhost:3000/weather?address=changping').then((response) => {
 				data.feel +
 				' degrees out.'
 		);
+	});
+}); */
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const location = search.value;
+	console.log('input location:' + location);
+	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				return console.log(data.error);
+			}
+			console.log(
+				data.cast +
+					'. It is currently ' +
+					data.temperature +
+					' degree out. It feels like ' +
+					data.feel +
+					' degrees out.'
+			);
+		});
 	});
 });
