0a1ba9b6b93587ac9db4afdbffc3e5886fe318e0
QuanLi <quanlidavid@gmail.com>
2022-02-02 15:17:28 +0800

    add some style for form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -24,16 +24,29 @@ fetch('http://localhost:3000/weather?address=changping').then((response) => {
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1'); //# for id; . for class
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (event) => {
 	event.preventDefault();
 	const location = search.value;
 	console.log('input location:' + location);
+	messageOne.textContent = 'Loading weather for ' + location;
+	messageTwo.textContent = '';
 	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
+				messageOne.textContent = data.error;
 				return console.log(data.error);
 			}
+			messageOne.textContent = location;
+			messageTwo.textContent =
+				data.cast +
+				'. It is currently ' +
+				data.temperature +
+				' degree out. It feels like ' +
+				data.feel +
+				' degrees out.';
 			console.log(
 				data.cast +
 					'. It is currently ' +
