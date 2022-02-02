4c87a25bda7923fd36d88c604d6467aed71852f2
QuanLi <quanlidavid@gmail.com>
2022-02-02 23:46:24 +0800

    update code for heroku deploy

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -31,7 +31,7 @@ weatherForm.addEventListener('submit', (event) => {
 	console.log('input location:' + location);
 	messageOne.textContent = 'Loading weather for ' + location;
 	messageTwo.textContent = '';
-	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+	fetch('/weather?address=' + location).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				messageOne.textContent = data.error;
