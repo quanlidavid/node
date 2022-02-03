55d47e3a5353481e7ab403da416bf0ac36618b5d
QuanLi <quanlidavid@gmail.com>
2022-02-03 21:27:31 +0800

    promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -21,13 +21,27 @@
 // 	console.log(data);
 // });
 
+// const add = (num1,num2,callback) => {
+//     setTimeout(() => {
+//         callback(num1 + num2)
+//     },2000)
+// }
 
-const add = (num1,num2,callback) => {
-    setTimeout(() => {
-        callback(num1 + num2)
-    },2000)
-}
+// add(1,4,(sum)=>{
+//     console.log(sum);
+// })
 
-add(1,4,(sum)=>{
-    console.log(sum);
-})
+const doWorkCallback = (callback) => {
+	setTimeout(() => {
+		// callback('This is my error!', undefined);
+		callback(undefined, [ 1, 4, 7 ]);
+	}, 2000);
+};
+
+doWorkCallback((error, result) => {
+	if (error) {
+		return console.log(error);
+	}
+	console.log(result);
+});
+
