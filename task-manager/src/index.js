bc6cf2d4e8bde508f4041cea63b684e66233ff29
QuanLi <quanlidavid@gmail.com>
2022-02-04 17:31:36 +0800

    create rest api "create task post /tasks"

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,19 +9,28 @@ const port = process.env.PORT || 3000;
 app.use(express.json()); //parses incoming requests with JSON payloads
 
 app.post('/users', (req, res) => {
-	const user = new User(req.body);
-	user
-		.save()
-		.then(() => {
-			res.send(user);
-		})
-		.catch((error) => {
-			res.status(400).send(error);
-		});
+    const user = new User(req.body);
+    user
+        .save()
+        .then(() => {
+            res.status(201).send(user);
+        })
+        .catch((error) => {
+            res.status(400).send(error);
+        });
 });
 
-app;
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+    task
+        .save()
+        .then(() => {
+            res.status(201).send(task);
+        }).catch((error) => {
+            res.status(400).send(error);
+        });
+});
 
 app.listen(port, () => {
-	console.log('Server is up on port ' + port);
+    console.log('Server is up on port ' + port);
 });
