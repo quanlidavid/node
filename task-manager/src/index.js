29d396ca038e061ab2ea560e8ea54f9bb83de5da
QuanLi <quanlidavid@gmail.com>
2022-02-04 18:45:00 +0800

    create rest api "read users/tasks"

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,28 +9,77 @@ const port = process.env.PORT || 3000;
 app.use(express.json()); //parses incoming requests with JSON payloads
 
 app.post('/users', (req, res) => {
-    const user = new User(req.body);
-    user
-        .save()
-        .then(() => {
-            res.status(201).send(user);
-        })
-        .catch((error) => {
-            res.status(400).send(error);
-        });
+	const user = new User(req.body);
+	user
+		.save()
+		.then(() => {
+			res.status(201).send(user);
+		})
+		.catch((error) => {
+			res.status(400).send(error);
+		});
+});
+
+app.get('/users', (req, res) => {
+	User.find({})
+		.then((users) => {
+			res.send(users);
+		})
+		.catch((error) => {
+			res.status(400).send(error);
+		});
+});
+
+app.get('/users/:id', (req, res) => {
+	const _id = req.params.id;
+	User.findById(_id)
+		.then((user) => {
+			if (!user) {
+				return res.status(404).send();
+			}
+			res.send(user);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
 });
 
 app.post('/tasks', (req, res) => {
-    const task = new Task(req.body);
-    task
-        .save()
-        .then(() => {
-            res.status(201).send(task);
-        }).catch((error) => {
-            res.status(400).send(error);
-        });
+	const task = new Task(req.body);
+	task
+		.save()
+		.then(() => {
+			res.status(201).send(task);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
+
+app.get('/tasks', (req, res) => {
+	Task.find({})
+		.then((tasks) => {
+			res.send(tasks);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
+
+app.get('/tasks/:id', (req, res) => {
+	const _id = req.params.id;
+	Task.findById(_id)
+		.then((task) => {
+			if (!task) {
+				return res.status(404).send();
+			}
+			res.send(task);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
 });
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port);
+	console.log('Server is up on port ' + port);
 });
