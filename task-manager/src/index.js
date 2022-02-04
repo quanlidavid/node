997dceefb64c1220730eac28d32dd09d88847d3c
QuanLi <quanlidavid@gmail.com>
2022-02-04 16:49:01 +0800

    create rest api "create /users"

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+require('./db/mongoose'); //will run mongoose.js to connecting database
+const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json()); //parses incoming requests with JSON payloads
+
+app.post('/users', (req, res) => {
+	const user = new User(req.body);
+	user
+		.save()
+		.then(() => {
+			res.send(user);
+		})
+		.catch((error) => {
+			res.status(400).send(error);
+		});
+});
+
+app;
 
 app.listen(port, () => {
 	console.log('Server is up on port ' + port);
