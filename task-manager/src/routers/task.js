80d3732ee2db78872148e93d9937da5f75c62ceb
QuanLi <quanlidavid@gmail.com>
2022-02-06 17:37:27 +0800

    sorting data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -16,10 +16,18 @@ router.post('/tasks', auth, async (req, res) => {
 	}
 });
 
+// GET /task?completed=true
+// GET /task?limit=10&skip=20
+// GET /task?sortBy=createdAt:asc
 router.get('/tasks', auth, async (req, res) => {
 	const match = {};
 	if (req.query.completed) {
 		match.completed = req.query.completed === 'true';
+	}
+	const sort = {};
+	if (req.query.sortBy) {
+		const parts = req.query.sortBy.split(':');
+		sort[parts[0]] = parts[1] === 'desc' ? -1 : 1; //descending is -1, ascending is 1
 	}
 	try {
 		// const tasks = await Task.find({ owner: req.user._id });
@@ -28,7 +36,8 @@ router.get('/tasks', auth, async (req, res) => {
 			match,
 			options: {
 				limit: parseInt(req.query.limit),
-				skip: parseInt(req.query.skip)
+				skip: parseInt(req.query.skip),
+				sort
 			}
 		});
 		res.send(req.user.tasks);
