e7fa43d660f46fa12f02d61946fcde3006f6b108
QuanLi <quanlidavid@gmail.com>
2022-02-09 22:25:01 +0800

    basic jest test

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math');
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math');
 
 test('Should calculate total with tip', () => {
 	const total = calculateTip(10, 0.3);
@@ -8,4 +8,9 @@ test('Should calculate total with default tip', () => {
 	const total = calculateTip(10);
 	expect(total).toBe(12.5);
 });
-
+test('Should convert 32 F to 0 C', () => {
+	expect(fahrenheitToCelsius(32)).toBe(0);
+});
+test('Should convert 0 C to 32 F', () => {
+	expect(celsiusToFahrenheit(0)).toBe(32);
+});
