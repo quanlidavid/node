054ded4d26bfb49e2d0cb20a1cdd6d1ae1624444
QuanLi <quanlidavid@gmail.com>
2022-02-13 00:23:16 +0800

    jest beforeEach

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,7 +1,7 @@
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const DOMAIN = "sandboxa15bc010b8f74ac6ab0ce54c6d84c714.mailgun.org";
+const DOMAIN = 'sandboxa15bc010b8f74ac6ab0ce54c6d84c714.mailgun.org';
 const mg = mailgun.client({
 	username: 'api',
 	key: process.env.MAILGUN_API_KEY
@@ -16,7 +16,7 @@ const datetime = new Date();
 const sendWelcomeEmail = (email, name) => {
 	mg.messages
 		.create(DOMAIN, {
-            from: "Mailgun Sandbox <postmaster@sandboxa15bc010b8f74ac6ab0ce54c6d84c714.mailgun.org>",
+			from: 'Mailgun Sandbox <postmaster@sandboxa15bc010b8f74ac6ab0ce54c6d84c714.mailgun.org>',
 			to: email,
 			subject: 'Thanks for joining in! ' + datetime.toLocaleDateString() + ' ' + datetime.toLocaleTimeString(),
 			text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
@@ -29,7 +29,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelEmail = (email, name) => {
 	mg.messages
 		.create(DOMAIN, {
-            from: "Mailgun Sandbox <postmaster@sandboxa15bc010b8f74ac6ab0ce54c6d84c714.mailgun.org>",
+			from: 'Mailgun Sandbox <postmaster@sandboxa15bc010b8f74ac6ab0ce54c6d84c714.mailgun.org>',
 			to: email,
 			subject: 'Sorry to see you go! ' + datetime.toLocaleDateString() + ' ' + datetime.toLocaleTimeString(),
 			text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,13 +1,44 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
 
-test('Should signup a new user', async () => {
+const userOne = {
+	name: 'John',
+	email: 'john@example.com',
+	password: '12345qwert'
+};
+beforeEach(async () => {
+	await User.deleteMany();
+	await new User(userOne).save();
+});
+
+// test('Should signup a new user', async () => {
+// 	await request(app)
+// 		.post('/users')
+// 		.send({
+// 			name: 'David-2-12-001',
+// 			email: 'davidsuger@163.com',
+// 			password: '123qaz888'
+// 		})
+// 		.expect(201);
+// });
+
+test('Should login existing user', async () => {
 	await request(app)
-		.post('/users')
+		.post('/users/login')
 		.send({
-			name: 'David-2-12-001',
-			email: 'davidsuger@163.com',
-			password: '123qaz888'
+			email: userOne.email,
+			password: userOne.password
 		})
-		.expect(201);
+		.expect(200);
+});
+
+test('Should not login nonexisting user', async () => {
+	await request(app)
+		.post('/users/login')
+		.send({
+			email: userOne.email,
+			password: 'badPassword'
+		})
+		.expect(400);
 });
