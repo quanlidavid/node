d768ce0955c8d11998def6c99bc9e9cbdc77789e
QuanLi <quanlidavid@gmail.com>
2022-02-16 00:07:28 +0800

    form and button states

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,14 +1,23 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (msg) => {
 	console.log(msg);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
 	event.preventDefault(); //防止页面刷新
 	// debugger;
+	$messageFormButton.setAttribute('disabled', 'disabled');
 	const message = event.target.elements.message.value;
 	socket.emit('sendMessage', message, (error) => {
+		$messageFormButton.removeAttribute('disabled');
+		$messageFormInput.value = '';
+		$messageFormInput.focus();
 		if (error) {
 			return console.log(error);
 		}
@@ -16,12 +25,13 @@ document.querySelector('#message-form').addEventListener('submit', (event) => {
 	});
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
 	// debugger;
 	if (!navigator.geolocation) {
 		return alert('Geolocation is not supported by your browser.');
 	}
-
+	$sendLocationButton.setAttribute('disabled', 'disabled');
+	
 	//No response if do not use proxy
 	navigator.geolocation.getCurrentPosition((position) => {
 		socket.emit(
@@ -29,6 +39,7 @@ document.querySelector('#send-location').addEventListener('click', () => {
 			{ latitude: position.coords.latitude, longitude: position.coords.longitude },
 			() => {
 				console.log('Location shared!');
+				$sendLocationButton.removeAttribute('disabled');
 			}
 		);
 	});
