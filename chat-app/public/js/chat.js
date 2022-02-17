0a2ff0b7b4bebb43cd42590e615a81e35c28a77e
QuanLi <quanlidavid@gmail.com>
2022-02-17 21:31:04 +0800

    render location message

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -8,11 +8,18 @@ const $messages = document.querySelector('#messages');
 
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
 
 socket.on('message', (msg) => {
 	console.log(msg);
 	// debugger
 	const html = Mustache.render(messageTemplate, { message: msg });
+	$messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('locationMessage', (url) => {
+	console.log(url);
+	const html = Mustache.render(locationMessageTemplate, { url });
 	$messages.insertAdjacentHTML('beforeend', html);
 });
 
